37f21c6f28705c7f95085067d0dd81e3c1506eb4
Arturs Girons <arturs.girons@gmail.com>
2016-02-12 15:03:55 +0200

    update

--- node/index.js
+++ node/index.js
@@ -3,7 +3,9 @@ module.exports = function() {
   var path = require('path');
   var app = express();
 
-  app.use(express.static(path.resolve(__dirname, '..', 'assets')))
+  var assetsPath = path.resolve(__dirname, path.relative(__dirname, './'), 'node_modules/node-pdfjs-viewer/assets')
+
+  app.use(express.static(assetsPath))
 
   return app
 }
